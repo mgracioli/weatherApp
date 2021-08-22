33bcbfbb592a67982ad221478ae8eb4a18c13e1e
mgracioli <michel-rg@hotmail.com>
2021-08-21 21:58:20 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# WeatherApp
+
+Aplicativo de Clima construído em Angular + api weather 
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.2.
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodayComponent } from './today/today.component';
+import { HttpClientModule } from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
